9c9544f93c115be78107074b586824a610ba67b4
Abdukhaligov <hikmat.pou@gmail.com>
2021-02-09 18:30:53 +0400

    Update User.php

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -20,7 +20,7 @@ class User extends Resource {
 
   public function fields(Request $request) {
     return [
-      ID::make()->sortable(),
+      ID::make(__('ID'), 'id')->sortable(),
 
       Gravatar::make()->maxWidth(50),
 
