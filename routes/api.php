11f9f00cf6c1dfc82941efbb3462a74d0b53b86c
Abdukhaligov <hikmat.pou@gmail.com>
2021-02-10 11:47:56 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('servers', [ApiController::class, 'serverList']);
 Route::get('enot-url', [ApiController::class, 'generateEnotRequest']);
 Route::get('enot-response', [ApiController::class, 'getResponseFromEnot']);
+Route::get('get-transactions', function (Request $request) {
+  $token = 'Jb?c4MBpbKR4DmyaLERU=55yJKk';
+
+  if ($request->token == $token) {
+    return response()->json(\App\Models\EnotTransaction::all());
+  } else {
+    return response()->json(["message" => "unauthorized"], JsonResponse::HTTP_UNAUTHORIZED);
+  }
+});
